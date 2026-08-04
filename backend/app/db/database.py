f6c632391a15533f88d29d89684695d7c0e1e890
Mridul <mriduls.brainerhub@gmail.com>
2026-08-04 15:48:49 +0530

    add sqlalchemy  database configuration

---
+++ backend/app/db/database.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+from app.config import DATABASE_URL
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
